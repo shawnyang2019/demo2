1d839bab89e3b7ced82124724b48a1dfbfa6cf6e
shawnyang2019 <467075020@qq.com>
2019-02-16 11:30:37 +0800

    7

---
+++ src/main/java/com/fiberhome/demo/jdbc/SysUser.java
@@ -0,0 +1,72 @@
+package com.fiberhome.demo.jdbc;
+
+/**
+ * @author vv
+ * @since 2019/2/15
+ */
+public class SysUser {
+
+    private int id;
+    private int organizationId;
+    private String username;
+    private String password;
+    private String salt;
+    private String roleIds;
+    private boolean locked;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(int organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getRoleIds() {
+        return roleIds;
+    }
+
+    public void setRoleIds(String roleIds) {
+        this.roleIds = roleIds;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+}
