2fae77e299d0b47eaab024030417415dd166d1ec
tom_glb <goodMorning_glb@atguigu.com>
2019-04-30 16:42:18 +0800

    2019/4/30 git&github learning test commit

---
+++ src/main/java/githubtest/Githubtest.java
@@ -0,0 +1,12 @@
+package githubtest;
+
+/**
+ * @author ShawnYang
+ * @create 2019-04-30 16:40
+ */
+public class Githubtest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
