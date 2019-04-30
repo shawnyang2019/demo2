5722e602c97bd5ff9917d82b9e5eea3814ecfdfc
tom_glb <goodMorning_glb@atguigu.com>
2019-04-30 21:40:10 +0800

    2019/4/30 git&github learning test commit   666666

--- src/main/java/githubtest/GithubTest2.java
+++ src/main/java/githubtest/GithubTest2.java
@@ -10,5 +10,6 @@ public class GithubTest2 {
 
     public static void main(String[] args) {
         System.out.println("githut test 2 1111111111111");
+        System.out.println("githut test 2 22222222222222");
     }
 }
---
+++ src/main/java/githubtest/GithubTest3.java
@@ -0,0 +1,11 @@
+package githubtest;
+
+/**
+ * @author ShawnYang
+ * @create 2019-04-30 21:38
+ */
+public class GithubTest3 {
+    public static void main(String[] args) {
+        System.out.println("shawn is the best");
+    }
+}
