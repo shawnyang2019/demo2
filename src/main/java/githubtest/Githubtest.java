6ed036c3e185a09eb1e9e21f3370e4cc4a9d628a
tom_glb <goodMorning_glb@atguigu.com>
2019-04-30 19:26:38 +0800

    2019/4/30 git&github learning test commit 2222

--- src/main/java/githubtest/Githubtest.java
+++ src/main/java/githubtest/Githubtest.java
@@ -7,6 +7,6 @@ package githubtest;
 public class Githubtest {
 
     public static void main(String[] args) {
-        System.out.println("hello,world");
+        System.out.println("hello,world2222");
     }
 }
